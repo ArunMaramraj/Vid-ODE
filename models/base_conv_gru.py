e46b0be8f1051031108d288a1c5b29fd17d95921
Arun Maramraj <imarun500@gmail.com>
2024-10-19 08:26:29 +0530

    Update base_conv_gru.py

--- models/base_conv_gru.py
+++ models/base_conv_gru.py
@@ -11,14 +11,6 @@ import utils
 
 class ConvGRUCell(nn.Module):
     def __init__(self, input_size, input_dim, hidden_dim, kernel_size, bias, dtype):
-        """
-        :param input_size: (int, int) / Height and width of input tensor as (height, width).
-        :param input_dim: int / Number of channels of input tensor.
-        :param hidden_dim: int / Number of channels of hidden state.
-        :param kernel_size: (int, int) / Size of the convolutional kernel.
-        :param bias: bool / Whether or not to add the bias.
-        :param dtype: torch.cuda.FloatTensor or torch.FloatTensor / Whether or not to use cuda.
-        """
         super(ConvGRUCell, self).__init__()
         self.height, self.width = input_size
         self.padding = kernel_size[0] // 2, kernel_size[1] // 2
@@ -42,12 +34,6 @@ class ConvGRUCell(nn.Module):
         return (torch.zeros(batch_size, self.hidden_dim, self.height, self.width)).type(self.dtype)
     
     def forward(self, input_tensor, h_cur, mask=None):
-        """
-        :param self:
-        :param input_tensor: (b, c, h, w) / input is actually the target_model
-        :param h_cur: (b, c_hidden, h, w) / current hidden and cell states respectively
-        :return: h_next, next hidden state
-        """
         combined = torch.cat([input_tensor, h_cur], dim=1)
         combined_conv = self.conv_gates(combined)
         
@@ -61,8 +47,9 @@ class ConvGRUCell(nn.Module):
         
         h_next = (1 - update_gate) * h_cur + update_gate * cnm
         
+        # Avoid in-place operation
         mask = mask.view(-1, 1, 1, 1).expand_as(h_cur)
-        h_next = mask * h_next + (1 - mask) * h_cur
+        h_next = mask * h_next.clone() + (1 - mask) * h_cur.clone()
         
         return h_next
 
@@ -74,7 +61,6 @@ class Encoder_z0_ODE_ConvGRU(nn.Module):
         
         super(Encoder_z0_ODE_ConvGRU, self).__init__()
         
-        # Make sure that both `kernel_size` and `hidden_dim` are lists having len == num_layers
         kernel_size = self._extend_for_multilayer(kernel_size, num_layers)
         hidden_dim = self._extend_for_multilayer(hidden_dim, num_layers)
         if not len(kernel_size) == len(hidden_dim) == num_layers:
@@ -92,7 +78,6 @@ class Encoder_z0_ODE_ConvGRU(nn.Module):
         self.z0_diffeq_solver = z0_diffeq_solver
         self.run_backwards = run_backwards
         
-        ##### By product for visualization
         self.by_product = {}
         
         cell_list = []
@@ -105,10 +90,8 @@ class Encoder_z0_ODE_ConvGRU(nn.Module):
                                          bias=self.bias,
                                          dtype=self.dtype))
         
-        # convert python list to pytorch module
         self.cell_list = nn.ModuleList(cell_list)
         
-        # last conv layer for generating mu, sigma
         self.z0_dim = hidden_dim[0]
         z = hidden_dim[0]
         self.transform_z0 = nn.Sequential(
@@ -119,7 +102,6 @@ class Encoder_z0_ODE_ConvGRU(nn.Module):
     def forward(self, input_tensor, time_steps, mask=None, tracker=None):
     
         if not self.batch_first:
-            # (t, b, c, h, w) -> (b, t, c, h, w)
             input_tensor = input_tensor.permute(1, 0, 2, 3, 4)
         
         assert (input_tensor.size(1) == len(time_steps)), "Sequence length should be same as time_steps"
@@ -144,11 +126,8 @@ class Encoder_z0_ODE_ConvGRU(nn.Module):
         
         device = utils.get_device(input_tensor)
         
-        # Set initial inputs
         prev_input_tensor = torch.zeros((b, c, h, w)).to(device)
         
-        # Time configuration
-        # Run ODE backwards and combine the y(t) estimates using gating
         prev_t, t_i = time_steps[-1] + 0.01, time_steps[-1]
         latent_ys = []
         
@@ -162,10 +141,10 @@ class Encoder_z0_ODE_ConvGRU(nn.Module):
             assert (not torch.isnan(inc).any())
             tracker.write_info(key=f"inc{idx}", value=inc.clone().cpu())
             
-            ode_sol = prev_input_tensor + inc
+            ode_sol = prev_input_tensor.clone() + inc
             tracker.write_info(key=f"prev_input_tensor{idx}", value=prev_input_tensor.clone().cpu())
             tracker.write_info(key=f"ode_sol{idx}", value=ode_sol.clone().cpu())
-            ode_sol = torch.stack((prev_input_tensor, ode_sol), dim=1)  # [1, b, 2, c, h, w] => [b, 2, c, h, w]
+            ode_sol = torch.stack((prev_input_tensor, ode_sol), dim=1)
             assert (not torch.isnan(ode_sol).any())
             
             if torch.mean(ode_sol[:, 0, :] - prev_input_tensor) >= 0.001:
@@ -176,13 +155,11 @@ class Encoder_z0_ODE_ConvGRU(nn.Module):
             yi_ode = ode_sol[:, -1, :]
             xi = input_tensor[:, i, :]
             
-            # only 1 now
             yi = self.cell_list[0](input_tensor=xi,
                                    h_cur=yi_ode,
                                    mask=mask[:, i])
             
-            # return to iteration
-            prev_input_tensor = yi
+            prev_input_tensor = yi.clone()
             prev_t, t_i = time_steps[i], time_steps[i - 1]
             latent_ys.append(yi)
         
@@ -199,64 +176,13 @@ class Encoder_z0_ODE_ConvGRU(nn.Module):
     @staticmethod
     def _check_kernel_size_consistency(kernel_size):
         if not (isinstance(kernel_size, tuple) or
-                (isinstance(kernel_size, list) and all([isinstance(elem, tuple) for elem in kernel_size]))):
-            raise ValueError('`kernel_size` must be tuple or list of tuples')
+                isinstance(kernel_size, list)):
+            raise ValueError('`kernel_size` must be tuple or list.')
+        if isinstance(kernel_size, list) and not all([isinstance(elem, tuple) for elem in kernel_size]):
+            raise ValueError('`kernel_size` must be list of tuples.')
     
     @staticmethod
     def _extend_for_multilayer(param, num_layers):
         if not isinstance(param, list):
             param = [param] * num_layers
         return param
-
-
-def get_norm_layer(ch):
-    norm_layer = nn.BatchNorm2d(ch)
-    return norm_layer
-
-
-class Encoder(nn.Module):
-    
-    def __init__(self, input_dim=3, ch=64, n_downs=2):
-        super(Encoder, self).__init__()
-        
-        model = []
-        model += [nn.Conv2d(input_dim, ch, 3, 1, 1)]
-        model += [get_norm_layer(ch)]
-        model += [nn.ReLU()]
-        
-        for _ in range(n_downs):
-            model += [nn.Conv2d(ch, ch * 2, 4, 2, 1)]
-            model += [get_norm_layer(ch * 2)]
-            model += [nn.ReLU()]
-            ch *= 2
-        
-        self.model = nn.Sequential(*model)
-    
-    def forward(self, x):
-        out = self.model(x)
-        return out
-
-
-class Decoder(nn.Module):
-    
-    def __init__(self, input_dim=256, output_dim=3, n_ups=2):
-        super(Decoder, self).__init__()
-        
-        model = []
-        
-        ch = input_dim
-        for i in range(n_ups):
-            model += [nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)]
-            model += [nn.Conv2d(ch, ch // 2, 3, 1, 1)]
-            model += [get_norm_layer(ch // 2)]
-            model += [nn.ReLU()]
-            ch = ch // 2
-        
-        model += [nn.Conv2d(ch, output_dim, 3, 1, 1)]
-        # model += [nn.Tanh()]
-        
-        self.model = nn.Sequential(*model)
-    
-    def forward(self, x):
-        out = self.model(x)
-        return out
