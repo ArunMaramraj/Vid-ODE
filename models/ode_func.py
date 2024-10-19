d37d5ae5df032b365df36dbb3477c666ad775806
Arun Maramraj <imarun500@gmail.com>
2024-10-19 08:32:49 +0530

    Update ode_func.py

--- models/ode_func.py
+++ models/ode_func.py
@@ -20,39 +20,40 @@ class DiffeqSolver(nn.Module):
     
     def forward(self, first_point, time_steps_to_predict, backwards=False):
         """
-		# Decode the trajectory through ODE Solver
-		"""
-        # n_traj_samples, n_traj = first_point.size()[0], first_point.size()[1]
-        
+        Decode the trajectory through ODE Solver
+        """
         pred_y = odeint(self.ode_func, first_point, time_steps_to_predict,
                         rtol=self.odeint_rtol, atol=self.odeint_atol, method=self.ode_method)
-        pred_y = pred_y.permute(1, 0, 2, 3, 4)  # => [b, t, c, h0, w0]
-
+        
+        # Ensure no in-place modification
+        pred_y = pred_y.permute(1, 0, 2, 3, 4).clone()  # => [b, t, c, h0, w0]
         
         return pred_y
     
     def sample_traj_from_prior(self, starting_point_enc, time_steps_to_predict, n_traj_samples=1):
         """
-		# Decode the trajectory through ODE Solver using samples from the prior
-
-		time_steps_to_predict: time steps at which we want to sample the new trajectory
-		"""
+        Decode the trajectory through ODE Solver using samples from the prior
+        """
         func = self.ode_func.sample_next_point_from_prior
         
         pred_y = odeint(func, starting_point_enc, time_steps_to_predict,
                         rtol=self.odeint_rtol, atol=self.odeint_atol, method=self.ode_method)
-        pred_y = pred_y.permute(1, 2, 0, 3)
+        
+        # Ensure no in-place modification
+        pred_y = pred_y.permute(1, 2, 0, 3).clone()
+        
         return pred_y
 
 
 #####################################################################################################
 
+
 class ODEFunc(nn.Module):
     def __init__(self, opt, input_dim, latent_dim, ode_func_net, device=torch.device("cpu")):
         """
-		input_dim: dimensionality of the input
-		latent_dim: dimensionality used for ODE. Analog of a continous latent state
-		"""
+        input_dim: dimensionality of the input
+        latent_dim: dimensionality used for ODE. Analog of a continuous latent state
+        """
         super(ODEFunc, self).__init__()
         
         self.input_dim = input_dim
@@ -63,22 +64,22 @@ class ODEFunc(nn.Module):
     
     def forward(self, t_local, y, backwards=False):
         """
-		Perform one step in solving ODE. Given current data point y and current time point t_local, returns gradient dy/dt at this time point
-		t_local: current time point
-		y: value at the current time point
-		"""
-        grad = self.get_ode_gradient_nn(t_local, y)
+        Perform one step in solving ODE. Given current data point y and current time point t_local, returns gradient dy/dt at this time point
+        """
+        grad = self.get_ode_gradient_nn(t_local, y).clone()  # Clone gradient
+        
         if backwards:
-            grad = -grad
+            grad = -grad.clone()  # Avoid in-place negation
+        
         return grad
     
     def get_ode_gradient_nn(self, t_local, y):
         output = self.gradient_net(y)
-        return output
+        return output.clone()  # Clone to avoid in-place ops
     
     def sample_next_point_from_prior(self, t_local, y):
         """
-		t_local: current time point
-		y: value at the current time point
-		"""
-        return self.get_ode_gradient_nn(t_local, y)
+        t_local: current time point
+        y: value at the current time point
+        """
+        return self.get_ode_gradient_nn(t_local, y).clone()  # Ensure cloning to avoid in-place modification
